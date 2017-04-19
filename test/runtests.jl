b93a6fab760b50895564a7a269a86aac558a2882
Jon Malmaud <malmaud@gmail.com>
2017-04-19 17:21:14 -0400

    Drop .4 support

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,7 @@ df = DataFrame(Dict(:name=>["a", "b"], :age=>[27, 30]))
 age = values(df[:age])
 age[2] = 31
 @test loc(df)[1, "age"] == 31
+
+
+df = read_csv(joinpath(dirname(@__FILE__), "test.csv"))
+@test isa(df, Pandas.DataFrame)
