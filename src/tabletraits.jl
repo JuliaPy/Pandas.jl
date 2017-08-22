065720c04f9e2af1adc0c877c1160cd67fff08ab
David Anthoff <anthoff@berkeley.edu>
2017-08-22 12:07:59 -0700

    Handle missing data in TableTraits implementation

--- src/tabletraits.jl
+++ src/tabletraits.jl
@@ -1,4 +1,5 @@
 using TableTraitsUtils
+import DataValues
 
 TableTraits.isiterable(x::DataFrame) = true
 TableTraits.isiterabletable(x::DataFrame) = true
@@ -14,5 +15,19 @@ end
 function _construct_pandas_from_iterabletable(source)
     columns, column_names = create_columns_from_iterabletable(source)
     cols = Dict(i[1]=>i[2] for i in zip(column_names, columns))
+
+    for (k,v) in cols
+        if eltype(v)<:DataValues.DataValue
+            T = eltype(eltype(v))
+            if T<:AbstractFloat
+                cols[k] = T[get(i, NaN) for i in v]
+            elseif T<:Integer
+                cols[k] = Float64[isnull(i) ? NaN : Float64(get(i)) for i in v]
+            else
+                throw(ArgumentError("Can't create a Pandas.DataFrame from a source that has missing data."))
+            end
+        end
+    end
+
     return DataFrame(cols)
 end
--- test/test_tabletraits.jl
+++ test/test_tabletraits.jl
@@ -1,5 +1,6 @@
 using Pandas
 using NamedTuples
+using DataValues
 using Base.Test
 
 @testset "TableTraits" begin
@@ -25,5 +26,23 @@ it_collected = collect(it)
 @test length(it_collected) == 2
 @test it_collected[1] == @NT(a=1, b="John", c=3.2)
 @test it_collected[2] == @NT(a=2, b="Sally", c=5.8)
+
+table_array2 = [@NT(a=1, b=DataValue("John"), c=3.2), @NT(a=2, b=DataValue("Sally"), c=5.8)]
+
+@test_throws ArgumentError DataFrame(table_array2)
+
+table_array3 = [@NT(a=DataValue{Int}(), b="John", c=DataValue(3.2)), @NT(a=DataValue(2), b="Sally", c=DataValue{Float64}())]
+
+df3 = DataFrame(table_array3)
+
+it3_collected = collect(TableTraits.getiterator(df3))
+
+@test length(it3_collected) == 2
+@test isnan(it3_collected[1].a)
+@test it3_collected[1].b == "John"
+@test it3_collected[1].c == 3.2
+@test it3_collected[2].a == 2
+@test it3_collected[2].b == "Sally"
+@test isnan(it3_collected[2].c)
 
 end
